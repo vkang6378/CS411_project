6cfde8c57c6c1cdb5bcbd1cc4f36741ac03ad8f6
vkang6378 <91505648+vkang6378@users.noreply.github.com>
2023-06-15 10:11:25 -0400

    Update server.js

--- prototype/server.js
+++ prototype/server.js
@@ -17,18 +17,18 @@ app.use(express.static('public'));
 // This function retrieves the token for a given user
 async function getOAuthTokenForUser(userId) {
   // and returning the stored token
-  // For this example, we'll return a placeholder token
+  // Return a placeholder token as an example
   return "placeholder-token";
 }
 
 app.get('/search', async (req, res) => {
   try {
     const searchTerm = req.query.term;
-    const userId = req.query.userId; // You'll need to get the user's ID from somewhere, perhaps it's included in the search request
+    const userId = req.query.userId; // Need to get the user's ID from somewhere
 
     const token = await getOAuthTokenForUser(userId);
     
-    // Now, you can use the token in your API requests
+    // Use the token in your API requests
     const response = await axios.get(`API_URL?keyword=${searchTerm}`, {
       headers: {
         Authorization: `Bearer ${token}` // Replace with actual authorization header required by the API
