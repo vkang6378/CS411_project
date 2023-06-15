72db178fcf56f6e3180acba90621bdc1d8bb769b
vkang6378 <91505648+vkang6378@users.noreply.github.com>
2023-06-15 10:27:11 -0400

    Update server.js

--- prototype/backend/server.js
+++ prototype/backend/server.js
@@ -1,45 +1,57 @@
+// Load environment variables
 require('dotenv').config();
 
+// Import required modules
 const express = require('express');
-const app = express();
-const axios = require('axios');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const session = require('express-session');
+const authRoutes = require('./auth');
+const userRoutes = require('./users');
 
-// Setup middleware
-app.use(cors());
+const app = express();
+const port = 3000;
+
+// Use JSON and CORS middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-// Static hosting for built files(change public to name of frontend file)
-app.use(express.static('public'));
+// Add user Session Handling
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
-// This function retrieves the token for a given user
-async function getOAuthTokenForUser(userId) {
-  // and returning the stored token
-  // Return a placeholder token as an example
-  return "placeholder-token";
-}
+// Use routes from other files
+app.use(authRoutes);
+app.use(userRoutes);
 
+// Add route to make the API request
+/*
 app.get('/search', async (req, res) => {
+  const searchTerm = req.query.term;
+
+  const url = `API_URL?keyword=${searchTerm}&apikey=${process.env.API_KEY}`;
+  
   try {
-    const searchTerm = req.query.term;
-    const userId = req.query.userId; // Need to get the user's ID from somewhere
-
-    const token = await getOAuthTokenForUser(userId);
-    
-    // Use the token in your API requests
-    const response = await axios.get(`API_URL?keyword=${searchTerm}`, {
-      headers: {
-        Authorization: `Bearer ${token}` // Replace with actual authorization header required by the API
-      }
-    });
-
+    const response = await axios.get(url);
     res.json(response.data);
-  } catch (err) {
-    res.json({ error: err.message });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    res.status(500).json({ error: 'An error occurred while fetching events' });
   }
 });
+
+app.post('/search', express.json(), async (req, res) => {
+    try {
+      const searchTerm = req.body.search;
+      const response = await axios.get(`API_URL?keyword=${searchTerm}&apikey=${process.env.API_KEY}`);
+      res.json(response.data);
+    } catch (err) {
+      res.json({ error: err.message });
+    }
+});
+*/
 
 // Start the server
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
